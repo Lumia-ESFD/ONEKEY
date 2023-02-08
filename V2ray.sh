d5e9ffc9448bf6645b1889febeaed19ff5502697
Lumia-ESFD <Lumia.Nokia638@hotmail.com>
2023-02-08 20:32:33 +0800

    HO-WFLJASLFDKJASFLJ

--- V2ray.sh
+++ V2ray.sh
@@ -4,7 +4,7 @@ wget --no-check-certificate -P /tmp https://git.histb.com/v2fly/v2ray-core/relea
 mkdir -p /tmp/v2ray
 unzip /tmp/v2ray-linux-arm32-v7a.zip -d /tmp/v2ray
 mkdir -p /usr/local/bin/v2ray
-cp /tmp/v2ray /usr/local/bin/v2ray
+cp /tmp/v2ray/v2ray /usr/local/bin/v2ray
 mkdir -p /usr/local/etc/v2ray
 cp /tmp/v2ray/config.json  /usr/local/etc/v2ray/config.json
 mkdir -p /usr/local/share/v2ray
