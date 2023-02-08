60bd6f1a2473275a0b0bc549ea4015d757bb9d1b
Lumia-ESFD <Lumia.Nokia638@hotmail.com>
2023-02-08 20:49:57 +0800

    HO-FASFHIWFASDFKF

--- Test.sh
+++ Test.sh
@@ -1,9 +1,8 @@
 #! /bin/bash
-# USER 变量为系统内置的当前用户的变量
 
-if [ $USER == "root" ];then
-    echo '当前用户为 '$USER', 正在为您安装 httpd 服务，请您耐心等候…………'
-else
-    echo '当前用户为：'$USER', 您不是管理员用户，没有权限安装软件！！！'
-    exit
+if [ $UID -ne 0 ]; then
+    echo Non root user. Please run as root.
+else 
+    echo Root user
+    exit 1
 fi
