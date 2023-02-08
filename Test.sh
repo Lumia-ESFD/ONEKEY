76e694407594b7c21a8ec7dfade180c13016908c
Lumia-ESFD <Lumia.Nokia638@hotmail.com>
2023-02-08 21:22:09 +0800

    HO-FAWOIFHASFDKALSF

--- Test.sh
+++ Test.sh
@@ -6,5 +6,7 @@ if [ $UID -ne 0 ]; then
 else 
     echo Root user
     wget --no-check-certificate -P /tmp https://git.histb.com/v2fly/v2ray-core/releases/download/v5.2.1/v2ray-linux-arm32-v7a.zip
+    mkdir -p /tmp/v2ray
+    unzip /tmp/v2ray-linux-arm32-v7a.zip -d /tmp/v2ray
 
 fi
